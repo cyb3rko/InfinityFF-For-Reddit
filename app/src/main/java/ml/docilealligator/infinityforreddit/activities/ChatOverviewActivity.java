2e98dc6664d48969b9945bb40ef4d0a8a5ef0330
KhoalaS <s4khleee@uni-trier.de>
2023-06-21 13:18:52 +0200

    working on chat activity

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/ChatOverviewActivity.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/ChatOverviewActivity.java
@@ -3,14 +3,26 @@ package ml.docilealligator.infinityforreddit.activities;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.appbar.AppBarLayout;
 
@@ -21,6 +33,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import ml.docilealligator.infinityforreddit.Infinity;
 import ml.docilealligator.infinityforreddit.R;
+import ml.docilealligator.infinityforreddit.adapters.OpenChatAdapter;
 import ml.docilealligator.infinityforreddit.bottomsheetfragments.PostLayoutBottomSheetFragment;
 import ml.docilealligator.infinityforreddit.customtheme.CustomThemeWrapper;
 import ml.docilealligator.infinityforreddit.customviews.slidr.Slidr;
@@ -39,8 +52,13 @@ public class ChatOverviewActivity extends BaseActivity{
     AppBarLayout appBarLayout;
     @BindView(R.id.toolbar_chat_overview_activity)
     Toolbar toolbar;
+    @BindView(R.id.activity_chat_overview_pager)
+    ViewPager2 viewPager;
 
     private FragmentManager fragmentManager;
+    DemoCollectionAdapter demoCollectionAdapter;
+
+
 
 
     @Override
@@ -75,6 +93,8 @@ public class ChatOverviewActivity extends BaseActivity{
             }
         }
 
+        lockSwipeRightToGoBack();
+
         toolbar.setTitle("Chats");
 
         setSupportActionBar(toolbar);
@@ -82,6 +102,9 @@ public class ChatOverviewActivity extends BaseActivity{
         setToolbarGoToTop(toolbar);
 
         fragmentManager = getSupportFragmentManager();
+        demoCollectionAdapter = new DemoCollectionAdapter(this);
+        viewPager.setAdapter(demoCollectionAdapter);
+        viewPager.setBackgroundColor(mCustomThemeWrapper.getBackgroundColor());
     }
 
     @Override
@@ -113,5 +136,58 @@ public class ChatOverviewActivity extends BaseActivity{
     @Override
     protected void applyCustomTheme() {
 
+    }
+
+    @Override
+    public void lockSwipeRightToGoBack() {
+        if (mSliderPanel != null) {
+            mSliderPanel.lock();
+        }
+    }
+
+    public class DemoCollectionAdapter extends FragmentStateAdapter {
+        public DemoCollectionAdapter(FragmentActivity fragment) {
+            super(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            // Return a NEW fragment instance in createFragment(int)
+            // create a different Fragment if in Request position
+            // fragment still needs impl
+            Fragment fragment = new DemoObjectFragment();
+            Bundle args = new Bundle();
+            args.putInt(DemoObjectFragment.ARG_OBJECT, position + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 1;
+        }
+    }
+
+    public static class DemoObjectFragment extends Fragment {
+        public static final String ARG_OBJECT = "object";
+        private RecyclerView.LayoutManager layoutManager;
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_chat_open, container, false);
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            Bundle args = getArguments();
+            String[] data = {"hello", "world"};
+            layoutManager = new LinearLayoutManager(this.getActivity());
+            ((RecyclerView) view.findViewById(R.id.fragment_chat_open_recyclerview)).setAdapter(new OpenChatAdapter(data));
+            ((RecyclerView) view.findViewById(R.id.fragment_chat_open_recyclerview)).setLayoutManager(layoutManager);
+
+        }
     }
 }
