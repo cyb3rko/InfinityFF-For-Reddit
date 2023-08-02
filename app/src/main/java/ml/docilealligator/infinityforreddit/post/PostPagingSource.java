156cbd2078fd2a5995e2f3414eceb6760cabcdd0
KhoalaS <s4khleee@uni-trier.de>
2023-08-02 14:55:33 +0200

    fix sortTime being ignored in Gql requests

--- app/src/main/java/ml/docilealligator/infinityforreddit/post/PostPagingSource.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/post/PostPagingSource.java
@@ -264,7 +264,7 @@ public class PostPagingSource extends ListenableFuturePagingSource<String, Post>
             afterKey = loadParams.getKey();
         }
         if(gqlAPI != null){
-            JSONObject data = createHomePostsVars(sortType.getType(), afterKey);
+            JSONObject data = createHomePostsVars(sortType.getType(), sortType.getTime(), afterKey);
             RequestBody body = RequestBody.create(data.toString(), okhttp3.MediaType.parse("application/json; charset=utf-8"));
             bestPost = gqlAPI.getBestPostsListenableFuture(APIUtils.getOAuthHeader(accessToken), body);
             pageFuture = Futures.transform(bestPost, this::transformDataGQL, executor);
@@ -282,7 +282,7 @@ public class PostPagingSource extends ListenableFuturePagingSource<String, Post>
                 IOException.class, LoadResult.Error::new, executor);
     }
 
-    private JSONObject createHomePostsVars(SortType.Type sortType, String lastItem){
+    private JSONObject createHomePostsVars(SortType.Type sortType, SortType.Time sortTime, String lastItem){
         JSONObject data = new JSONObject();
         try{
             data.put("id", "769ee26e130d");
@@ -312,6 +312,9 @@ public class PostPagingSource extends ListenableFuturePagingSource<String, Post>
             variables.put("interestTopicIds", new JSONArray());
             variables.put("pageSize", 15);
             variables.put("sort", sortType.value.toUpperCase(Locale.ROOT));
+            if(sortTime != null){
+                variables.put("range", sortTime.value.toUpperCase(Locale.ROOT));
+            }
 
 
             data.put("variables", variables);
@@ -330,6 +333,9 @@ public class PostPagingSource extends ListenableFuturePagingSource<String, Post>
             variables.put("subredditName", subredditName);
             variables.put("sort", sortType.value.toUpperCase(Locale.ROOT));
 
+            if(sortTime != null){
+                variables.put("range", sortTime.value.toUpperCase(Locale.ROOT));
+            }
             if(lastItem != null){
                 variables.put("after", lastItem);
             }
